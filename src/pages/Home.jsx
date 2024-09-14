e97c7606a1c3b663585e5d9ed06c214c0102006a
HSINYU LIN <114204648+starfish7982-crypto@users.noreply.github.com>
2024-09-14 15:30:08 -0400

    Not Showing 3D with group not recognize issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 
-import {Suspense} from "react";
-import { Canvas } from '@react-three/fiber';
+import { Suspense } from "react";
+import { Canvas} from '@react-three/fiber';
 import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   POPUP
@@ -10,13 +11,20 @@ import Loader from "../components/Loader";
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
-      {/* <Canvas 
+      <Canvas 
       className="w-full h-screen bg-transparent"
       camera={{near:0.1,far:1000}}
       >
 
       </Canvas>
-      <Suspense fallback={<Loader/>}></Suspense> */}
+      <Suspense fallback={<Loader/>}>
+        <directionalLight/>
+        <ambientLight/>
+        <pointLight/>
+        <spotLight/>
+        <hemisphereLight/>
+        <Island/>
+      </Suspense>
     </section>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.glb'],
   server: {
     host: true,  // 允许通过网络访问
     port: 3000,   // 设置端口为3000
