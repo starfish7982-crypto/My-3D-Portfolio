4076b472fd9916fdf755aa3e057740a01448b33c
HSINYU LIN <114204648+starfish7982-crypto@users.noreply.github.com>
2024-09-16 16:37:05 -0400

    Modifed Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,21 @@
-import React, { useRef, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import emailjs from '@emailjs/browser'
+import { Canvas } from "@react-three/fiber";
+
+import Fox from '../models/Fox';
+// import { Alert,Loader } from "../components";
+import Loader from "../components/Loader";
+import Alert from "../components/Alert";
+import useAlert from "../hooks/useAlert";
 
 
 const Contact = () => {
-  const formRef = useRef(null);
+  const formRef = useRef();
   const [form,setForm] = useState({name:"",email:"",message:""});
   const [isLoading,setIsLoading] = useState(false);
+  const [currentAnimation,setCurrentAnimation] = useState('idle');
+
+  const {alert, showAlert,hideAlert} = useAlert();
 
   const handleChange = (e) =>{
     setForm({...form,[e.target.name]:e.target.value})
@@ -13,6 +23,7 @@ const Contact = () => {
   const handleSumit = (e)=>{
     e.preventDefault();
     setIsLoading(true);
+    setCurrentAnimation('hit');
 
     emailjs.send(
      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
@@ -26,22 +37,28 @@ const Contact = () => {
      },
      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
     ).then(()=>{
-      setIsLoading(flase);
-      //TODO: Show success message
-      //TODO: Hide an alert
-      setForm({name:'',email:'',message:''});
+      setIsLoading(false);
+      showAlert({show:true,text:'Thank you for your message 😃',type:'success'});
+      setTimeout(()=>{
+        hideAlert(false);
+        setCurrentAnimation('idle');
+        setForm({name:'',email:'',message:''});
+      },[3000]);
+
     }).catch((error)=>{
       setIsLoading(false);
+      setCurrentAnimation('idle');
       console.log(error);
-      //TODO:Show error message
+      showAlert({show:true,text:'I didnt receive your message 😢',type:'danger'});
     })
   };
-  const handleFocus = (e) =>{
-  };
-  const handleBlur = () =>{};
+  const handleFocus = () => setCurrentAnimation('walk');
+  const handleBlur = () => setCurrentAnimation('idle');
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert}/>}
+
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
@@ -101,8 +118,37 @@ const Contact = () => {
           </button>
         </form>
       </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{
+            position:[0,0,5],
+            fov:75,
+            near: 0.1,
+            far: 1000,
+          }}
+        >
+          <directionalLight position={[0, 0, 1]} intensity={2.5} />
+          <ambientLight intensity={1} />
+          <pointLight position={[5, 10, 0]} intensity={2} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+         <Suspense fallback={<Loader/>}>  
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.629, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Contact
